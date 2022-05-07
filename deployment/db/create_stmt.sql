526e9018851238c60cfe8faeeb2ca24600e1cdaf
Min-Hua Chao <s106034803@m106.nthu.edu.tw>
2022-05-07 11:52:02 +0800

    update create_stmt.sql

--- deployment/db/create_stmt.sql
+++ deployment/db/create_stmt.sql
@@ -1,32 +1,32 @@
 CREATE TABLE `doc_file` (
-  `uid` varchar(45) COLLATE utf8_bin NOT NULL,
-  `path` varchar(200) COLLATE utf8_bin NOT NULL,
-  `file_name` varchar(45) COLLATE utf8_bin NOT NULL,
-  `object_create_time` varchar(45) COLLATE utf8_bin NOT NULL,
-  `object_update_time` varchar(45) COLLATE utf8_bin NOT NULL,
+  `uid` varchar(45) NOT NULL,
+  `path` varchar(200) DEFAULT NULL,
+  `file_name` varchar(45) DEFAULT NULL,
+  `object_create_time` varchar(45) DEFAULT NULL,
+  `object_update_time` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ;
 
 CREATE TABLE `obj_seq` (
   `obj_key` varchar(45) NOT NULL,
-  `obj_index` bigint(20) DEFAULT NULL,
+  `obj_index` bigint DEFAULT NULL,
   PRIMARY KEY (`obj_key`)
 ) ;
 
 CREATE TABLE `sys_attr` (
-  `uid` varchar(45) COLLATE utf8_bin NOT NULL,
-  `type_idx` tinyint(4) DEFAULT NULL,
-  `key` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `value` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `object_create_time` bigint(20) NOT NULL,
-  `object_update_time` bigint(20) NOT NULL,
+  `uid` varchar(45) NOT NULL,
+  `type_idx` tinyint DEFAULT NULL,
+  `key` varchar(45) DEFAULT NULL,
+  `value` varchar(45) DEFAULT NULL,
+  `object_create_time` bigint DEFAULT NULL,
+  `object_update_time` bigint DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ;
 
 CREATE TABLE `system_seq` (
-  `item_id` varchar(45) COLLATE utf8_bin NOT NULL,
-  `current_num` bigint(20) DEFAULT NULL,
-  `last_num` bigint(20) DEFAULT NULL,
-  `max_num` bigint(20) DEFAULT NULL,
+  `item_id` varchar(45) NOT NULL,
+  `current_num` bigint DEFAULT NULL,
+  `last_num` bigint DEFAULT NULL,
+  `max_num` bigint DEFAULT NULL,
   PRIMARY KEY (`item_id`)
 ) ;
