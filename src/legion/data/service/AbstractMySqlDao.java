8211088fee8802cb432c17cbb4bd3ee82fb8a30d
Min-Hua Chao <45264512+legion625@users.noreply.github.com>
2022-11-05 21:12:57 +0800

    [change]
    1. AbstractMySqlDao
      -1. 擴充QueryOperation。
      -2. 擴充Skewer。
      -3. DbColumn新增字串長度限制檢查。

--- src/legion/data/service/AbstractMySqlDao.java
+++ src/legion/data/service/AbstractMySqlDao.java
@@ -15,7 +15,10 @@ import java.util.stream.Collectors;
 import org.slf4j.event.Level;
 
 import legion.ObjectModel;
+import legion.ObjectSkewer;
 import legion.data.AbstractDao;
+import legion.data.skewer.TableColPack;
+import legion.data.skewer.TableRel;
 import legion.util.DataFO;
 import legion.util.DatabaseFO;
 import legion.util.LogUtil;
@@ -397,7 +400,7 @@ public class AbstractMySqlDao extends AbstractDao {
 		ResultSet rs = null;
 
 		try {
-			List<Object> datas = new ArrayList<>();
+			ArrayList<Object> datas = new ArrayList<>();
 			// joinTable/固定條件
 			String wstr = "where " + _nonNullCol + " is not null";
 			// 使用者定義的查詢條件
@@ -472,7 +475,8 @@ public class AbstractMySqlDao extends AbstractDao {
 		return _param;
 	}
 
-	protected static String packMasterQueryField(String _targetMasterField, String _masterTable, String _masterKey, String _detailTable, String _detailMasterKey) {
+	protected static String packMasterQueryField(String _targetMasterField, String _masterTable, String _masterKey,
+			String _detailTable, String _detailMasterKey) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("(");
 		sb.append("select ").append(_targetMasterField).append(" from ").append(_masterTable);
@@ -480,118 +484,412 @@ public class AbstractMySqlDao extends AbstractDao {
 		sb.append(")");
 		return sb.toString();
 	}
-	
-	protected static String packDetailQueryField(String _targetDetailField, String _masterTable,String _masterKey,String _detailTable, String _detailMasterKey) {
-		return packDetailQueryField( _targetDetailField,  _masterTable, _masterKey, _detailTable,  _detailMasterKey, null); 
+
+	protected static String packDetailQueryField(String _targetDetailField, String _masterTable, String _masterKey,
+			String _detailTable, String _detailMasterKey) {
+		return packDetailQueryField(_targetDetailField, _masterTable, _masterKey, _detailTable, _detailMasterKey, null);
 	}
 
 	protected static String packDetailQueryField(String _targetDetailField, String _masterTable, String _masterKey,
 			String _detailTable, String _detailMasterKey, Map<String, String> _fixConditionMap) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("(");
-		sb.append("select group_concat( distinct( ").append(_targetDetailField).append(" )) from ").append(_detailTable);
+		sb.append("select group_concat( distinct( ").append(_targetDetailField).append(" )) from ")
+				.append(_detailTable);
 		sb.append(" where ").append(_detailMasterKey).append(" = ").append(_masterTable).append(".").append(_masterKey);
-		if(_fixConditionMap!=null)
-			for(String _key: _fixConditionMap.keySet())
+		if (_fixConditionMap != null)
+			for (String _key : _fixConditionMap.keySet())
 				sb.append(" and ").append(_key).append(" = ").append(_fixConditionMap.get(_key));
 		sb.append(")");
 		return sb.toString();
 	}
-	
-	protected static String packConjQueryField(String _targetField2, String _table1, String _table2, String _conjTable, String _conjUid1, String _conjUid2) {
-		return packConjQueryField( _targetField2,  _table1,  _table2,  _conjTable,  _conjUid1,  _conjUid2, null);
+
+	protected static String packConjQueryField(String _targetField2, String _table1, String _table2, String _conjTable,
+			String _conjUid1, String _conjUid2) {
+		return packConjQueryField(_targetField2, _table1, _table2, _conjTable, _conjUid1, _conjUid2, null);
 	}
-	
-	protected static String packConjQueryField(String _targetField2, String _table1, String _table2, String _conjTable, String _conjUid1, String _conjUid2
-			, Map<String, String> _conjFixConditionMap) {
+
+	protected static String packConjQueryField(String _targetField2, String _table1, String _table2, String _conjTable,
+			String _conjUid1, String _conjUid2, Map<String, String> _conjFixConditionMap) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("(");
 		sb.append("select group_concat( distinct( ").append(_targetField2).append(" )) from ").append(_table2);
 		sb.append(" where ").append(COL_UID).append(" in (");
-		sb.append("select ").append(_conjUid2).append(" from ").append(_conjTable).append(" where ").append(_conjUid1).append(" = ").append(_table1).append(".").append(COL_UID);
+		sb.append("select ").append(_conjUid2).append(" from ").append(_conjTable).append(" where ").append(_conjUid1)
+				.append(" = ").append(_table1).append(".").append(COL_UID);
 		/* ConjFixConditionMap */
-		if(_conjFixConditionMap!=null)
-			for(String _key: _conjFixConditionMap.keySet()) {
+		if (_conjFixConditionMap != null)
+			for (String _key : _conjFixConditionMap.keySet()) {
 				sb.append(" and ").append(_key).append(" = ").append(_conjFixConditionMap.get(_key));
 			}
 		sb.append(")");
 		return sb.toString();
 	}
-	
-	private static <Q extends QueryParam, T> String combineQueryValue(Function<Q, String> _queryParamMappingParser, QueryValue<Q, T>[] _qvs, ConjunctiveOp _qvsConjunctiveOp) {
-		Function<QueryValue<Q, ?>, String> sqlParser = t->{
+
+	private static <Q extends QueryParam, T> String combineQueryValue(Function<Q, String> _queryParamMappingParser,
+			QueryValue<Q, T>[] _qvs, ConjunctiveOp _qvsConjunctiveOp) {
+		Function<QueryValue<Q, ?>, String> sqlParser = t -> {
 			StringBuilder sqlSb = new StringBuilder();
 			sqlSb.append(_queryParamMappingParser.apply(t.getCondition()));
 			Object v = t.getValue().orElse(null);
-			if(v==null)
+			if (v == null)
 				sqlSb.append(" is null");
 			else {
 				sqlSb.append(" ").append(t.getCompareOp()).append(" ");
-				if(v.getClass() == String.class) {
-					// TODO sqlSb.append("'").append( XssUtil.stripSqlInjection( (String)v)).append("'"); 
-					sqlSb.append("'").append((String)v).append("'");
-				}else
+				if (v.getClass() == String.class) {
+					// TODO sqlSb.append("'").append( XssUtil.stripSqlInjection(
+					// (String)v)).append("'");
+					sqlSb.append("'").append((String) v).append("'");
+				} else
 					sqlSb.append(v);
 			}
 			return sqlSb.toString();
 		};
 		StringBuilder sb = new StringBuilder();
 		boolean b = false;
-		for(QueryValue<Q, T> _qv:_qvs) {
-			if(b)
+		for (QueryValue<Q, T> _qv : _qvs) {
+			if (b)
 //				sb.append(" and ");
 				sb.append(" ").append(_qvsConjunctiveOp.getId()).append(" ");
-			else b = true;
+			else
+				b = true;
 			sb.append(sqlParser.apply(_qv));
 		}
 		return sb.toString();
 	}
-	
-	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1, String _keyCol1, String _tb2, String _keyCol2
-			, Function<Q, String> _queryParamMappingParser, Map<P, QueryValue[]> _inSelectQueryValueMap, P _p) {
-		return packExistsField(_tb1, _keyCol1, _tb2, _keyCol2, _queryParamMappingParser, _inSelectQueryValueMap, ConjunctiveOp.and, _p);
+
+	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1,
+			String _keyCol1, String _tb2, String _keyCol2, Function<Q, String> _queryParamMappingParser,
+			Map<P, QueryValue[]> _inSelectQueryValueMap, P _p) {
+		return packExistsField(_tb1, _keyCol1, _tb2, _keyCol2, _queryParamMappingParser, _inSelectQueryValueMap,
+				ConjunctiveOp.and, _p);
 	}
-	
-	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1, String _keyCol1, String _tb2, String _keyCol2
-			, Function<Q, String> _queryParamMappingParser, Map<P, QueryValue[]> _inSelectQueryValueMap,
-			ConjunctiveOp _qvsConjunctiveOp,
-			P _p) {
+
+	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1,
+			String _keyCol1, String _tb2, String _keyCol2, Function<Q, String> _queryParamMappingParser,
+			Map<P, QueryValue[]> _inSelectQueryValueMap, ConjunctiveOp _qvsConjunctiveOp, P _p) {
 		String conditionSql = "";
-		if(_inSelectQueryValueMap!=null) {
+		if (_inSelectQueryValueMap != null) {
 			QueryValue[] _queryValues = _inSelectQueryValueMap.get(_p);
-			conditionSql = _queryValues==null?"":combineQueryValue(_queryParamMappingParser, _queryValues, _qvsConjunctiveOp);
+			conditionSql = _queryValues == null ? ""
+					: combineQueryValue(_queryParamMappingParser, _queryValues, _qvsConjunctiveOp);
 		}
 		return packExistsField(_tb1, _keyCol1, _tb2, _keyCol2, conditionSql);
 	}
-	
-	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1, String _keyCol1, String _tb2, String _keyCol2
-			, String _conditionSql) {
-		return packExistsField( _tb1,  _keyCol1,  _tb2,  _keyCol2
-				,  _conditionSql, true);
+
+	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1,
+			String _keyCol1, String _tb2, String _keyCol2, String _conditionSql) {
+		return packExistsField(_tb1, _keyCol1, _tb2, _keyCol2, _conditionSql, true);
 	}
-	
-	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1, String _keyCol1, String _tb2, String _keyCol2
-			, String _conditionSql, boolean _equalTrue) {
+
+	protected static <Q extends QueryParam, T, P extends QueryParam> String packExistsField(String _tb1,
+			String _keyCol1, String _tb2, String _keyCol2, String _conditionSql, boolean _equalTrue) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(" exists (select 1 from ").append(_tb2);
 		sb.append(" where ").append(_keyCol2).append(" = ").append(_tb1).append(".").append(_keyCol1);
-		if(!DataFO.isEmptyString(_conditionSql)) {
+		if (!DataFO.isEmptyString(_conditionSql)) {
 			sb.append(" and ").append(_conditionSql);
-			if(_equalTrue)
+			if (_equalTrue)
 				sb.append(" = true");
-			else 
+			else
 				sb.append(" = false");
 		}
 		sb.append(")");
 		return sb.toString();
 	}
-	
+
 	protected static String packYearField(String _colTimeL) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("year(").append("from_unixtime(").append(_colTimeL).append("/1000)").append(")");
 		return sb.toString();
 	}
+
+	// -------------------------------------------------------------------------------
+	// ------------------------------------Skewer-------------------------------------
+	protected <T extends ObjectSkewer> T loadSkewer(TableColPack[] _tableColPacks, TableRel[] _tableRels, String _col,
+			String _value, Function<ResultSet, T> _fnParseObj) {
+		String sql = packLoadSkewerSql(_tableColPacks, _tableRels, _col, _value);
+		return loadSkewerBySql(sql, _fnParseObj);
+	}
+
+	protected <T extends ObjectSkewer> T loadSkewerBySql(String _sql, Function<ResultSet, T> _fnParseObj) {
+		Connection conn = getConn();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		T t = null;
+		try {
+			String sql = _sql;
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if (rs.next())
+				t = _fnParseObj.apply(rs);
+		} catch (Throwable e) {
+			LogUtil.log(log, e, Level.ERROR);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Throwable _e) {
+				LogUtil.log(log, _e, Level.ERROR);
+			}
+		}
+		return t;
+	}
+
+	protected <T extends ObjectSkewer> List<T> loadSkewerList(TableColPack[] _tableColPacks, TableRel[] _tableRels,
+			String _col, String _value, Function<ResultSet, T> _fnParseObj) {
+		String sql = packLoadSkewerSql(_tableColPacks, _tableRels, _col, _value);
+		return loadSkewerListBySql(sql, _fnParseObj);
+	}
+
+	protected <T extends ObjectSkewer> List<T> loadSkewerListBySql(String _sql, Function<ResultSet, T> _fnParseObj) {
+		List<T> list = new ArrayList<>();
+		if (DataFO.isEmptyString(_sql))
+			return list;
+
+		Connection conn = getConn();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			String sql = _sql;
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next())
+				list.add(_fnParseObj.apply(rs));
+		} catch (Throwable e) {
+			LogUtil.log(log, e, Level.ERROR);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Throwable _e) {
+				LogUtil.log(log, _e, Level.ERROR);
+			}
+		}
+		return list;
+	}
+
 	
+	protected <Q extends QueryParam, T extends ObjectSkewer> QueryOperation<Q, T> searchSkewer(
+			TableColPack[] _tableColPacks, TableRel[] _tableRels,
+			QueryOperation<Q, T> _param, Function<Q, String> _queryParamMappingParser,
+			Function<ResultSet, T> _fnParseObj) {
+		Function<QueryValue<Q,?>, String>  sqlParser = t->_queryParamMappingParser.apply(t.getCondition())+" " +t.getCompareOp()+" ? ";
+		return searchSkewer(_tableColPacks, _tableRels, _param, sqlParser, _queryParamMappingParser, _fnParseObj);
+	}
+	
+	protected <Q extends QueryParam, T extends ObjectSkewer> QueryOperation<Q, T> searchSkewer(
+			TableColPack[] _tableColPacks, TableRel[] _tableRels,
+			QueryOperation<Q, T> _param, Function<QueryValue<Q,?>, String> _sqlParser , Function<Q, String> _queryParamMappingParser,
+			Function<ResultSet, T> _fnParseObj){
+		List<T> resultList = new ArrayList<>();
+		if(_param==null)
+			return null;
+		
+		// TableColPacks
+		if(_tableColPacks==null || _tableColPacks.length<=1)
+			return null;
+		int mainCount = 0;
+		for(TableColPack _tcp: _tableColPacks)
+			if(_tcp.isMain())
+				mainCount++;
+		if(mainCount!=1) {
+			log.error("mainCount[{}] error.", mainCount);
+			return null;
+		}
+		
+		// TableRels
+		if(_tableRels==null || _tableRels.length<=0)
+			return null;
+		
+		Connection conn = getConn();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			ArrayList<Object> datas = new ArrayList<>();
+			String qstr = packSearchSkewerSql(datas, _tableColPacks, _tableRels, _param, _sqlParser, _queryParamMappingParser);
+			StringBuilder sb = new StringBuilder(qstr);
+			
+			// sort
+			String sort = _param.combineSorts(_queryParamMappingParser);
+			if(!DataFO.isEmptyString(sort))
+				sb.append(" order by ").append(sort);
+			
+			// limit
+			int[] limit = _param.getLimit();
+			boolean appendLimit = false;
+			if(limit[0]>=0 && limit[1] >=0) {
+				sb.append(" limit ").append(limit[0]).append(",").append(limit[1]);
+				appendLimit = true;
+			}
+			
+			qstr = sb.toString();
+			log.debug("sql: {}", qstr);
+			
+			pstmt = conn.prepareStatement(qstr);
+			int i=1;
+			for(Object data: datas)
+				pstmt.setObject(i++,data);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				resultList.add(_fnParseObj.apply(rs));
+			}
+			_param.setQueryResult(resultList);
+			
+			rs.close();
+			rs = null;
+			pstmt.close();
+			pstmt = null;
+			
+			/* set total */
+			if(appendLimit) {
+				StringBuilder sbTotal = new StringBuilder();
+				sbTotal.append(packSearchSkewerCountSql(datas, _tableColPacks, _tableRels, _param, _sqlParser, _queryParamMappingParser));
+				// 查詢總數
+				pstmt =conn.prepareStatement(sbTotal.toString());
+				
+				i=1;
+				for(Object data:datas) {
+					log.debug("{}\t{}", i, data);
+					pstmt.setObject(i++, data);
+				}
+				
+				rs = pstmt.executeQuery();
+				if(rs.next())
+					_param.setTotal(rs.getInt(1));
+				else
+					_param.setTotal(0);
+			}else
+				_param.setTotal(resultList.size());
+		}catch (Throwable e) {
+			LogUtil.log(log, e, Level.ERROR);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Throwable _e) {
+				LogUtil.log(log, _e, Level.ERROR);
+			}
+		}
+		return _param;
+	}
+	
+	// -------------------------------------------------------------------------------
+	protected final String packLoadSkewerSql(TableColPack[] _tableColPacks, TableRel[] _tableRels) {
+		try {
+			return packSearchSkewerSql(null, _tableColPacks, _tableRels, null, null, null);
+		} catch (Throwable e) {
+			LogUtil.log(log, e, Level.ERROR);
+			return null;
+		}
+	}
+	
+	private String packLoadSkewerSql(TableColPack[] _tableColPacks, TableRel[] _tableRels, String _col, String _value) {
+		String sql = packLoadSkewerSql(_tableColPacks, _tableRels);
+		StringBuilder sb = new StringBuilder(sql);
+		sb.append(" and ").append(_col).append(" = ").append(_value).append("'");
+		sql = sb.toString();
+		return sql;
+	}
+
+	private <Q extends QueryParam, T extends ObjectSkewer> String packSearchSkewerSql(ArrayList<Object> _datas, TableColPack[] _tableColPacks, TableRel[] _tableRels,
+			QueryOperation<Q, T> _param, Function<QueryValue<Q,?>, String> _sqlParser, Function<Q, String> _queryParamMappingParser
+			) throws Exception{
+		// joinTable/固定條件
+		String wstr = "where";
+		String relStr = "";
+		for (TableRel rel : _tableRels) {
+			if (!DataFO.isEmptyString(relStr))
+				relStr += " and";
+			relStr+=" " +rel.getTcp1().getAlias()+"."+rel.getCol1()+" = "+rel.getTcp2().getAlias()+"."+rel.getCol2();
+		}
+		wstr +=relStr;
+		
+		// 使用者定義的查詢條件
+		if(_datas !=null && _param !=null && _sqlParser!=null && _queryParamMappingParser!=null)
+			wstr = _param.combineConditions(_datas, wstr, _sqlParser, _queryParamMappingParser);
+		
+		// 主要查詢SQL
+		String qstr = "select ";
+		int i;
+		/* col */
+		for(TableColPack tcp: _tableColPacks) {
+			if(tcp.isMain())
+				qstr += tcp.getAlias() + ".*, ";
+			else {
+				for (String col : tcp.getCols())
+					qstr += tcp.getAlias() + "." + col + " as " + tcp.getNewCol(col) + ", ";
+			}
+		}
+		i = qstr.lastIndexOf(',');
+		if(i>0)
+			qstr = qstr.substring(0,i);
+		qstr += "from";
+		
+		/* table */
+		for(TableColPack tcp: _tableColPacks)
+			qstr +=" " +tcp.getTable()+" "+tcp.getAlias()+",";
+		i = qstr.lastIndexOf(',');
+		if(i>0)
+			qstr = qstr.substring(0,i);
+		
+		qstr+= " "+wstr;
+		
+		return qstr;
+	}
+	
+	private <Q extends QueryParam, T extends ObjectSkewer> String packSearchSkewerCountSql(ArrayList<Object> _datas, TableColPack[] _tableColPacks, TableRel[] _tableRels,
+			QueryOperation<Q, T> _param, Function<QueryValue<Q,?>, String> _sqlParser, Function<Q, String> _queryParamMappingParser){
+		// joinTable/固定條件
+		String wstr = "where";
+		String relStr = "";
+		for (TableRel rel : _tableRels) {
+			if (!DataFO.isEmptyString(relStr))
+				relStr += " and";
+			relStr+=" " +rel.getTcp1().getAlias()+"."+rel.getCol1()+" = "+rel.getTcp2().getAlias()+"."+rel.getCol2();
+		}
+		wstr +=relStr;
+		
+		// 使用者定義的查詢條件
+		if(_datas !=null && _param !=null && _sqlParser!=null && _queryParamMappingParser!=null) {
+			ArrayList<Object> datasCopy = new ArrayList<>(_datas);
+			wstr = _param.combineConditions(datasCopy, wstr, _sqlParser, _queryParamMappingParser);
+		}
+		
+		// 主要查詢SQL
+		String qstr = "select count(*) from";
+		int i;
+		/* table */
+		for(TableColPack tcp: _tableColPacks) {
+			qstr += " "+tcp.getTable() + " "+tcp.getAlias() +",";
+		}
+		i = qstr.lastIndexOf(',');
+		if(i>0)
+			qstr = qstr.substring(0,i);
+		
+		qstr+= " "+wstr;
+		
+		return qstr;
+	}
+	
+	
+	// -------------------------------------------------------------------------------
+	// ------------------------------------SkewerL------------------------------------
+
 	// -------------------------------------------------------------------------------
 	// -------------------------------------close-------------------------------------
 	protected final void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
